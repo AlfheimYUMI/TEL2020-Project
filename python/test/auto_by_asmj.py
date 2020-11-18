5e45e4799f944960d2e9e4cc5366cd02c4952fed
asmj122121 <54708710+asmj122121@users.noreply.github.com>
2020-11-18 15:40:25 +0800

    edit auto_by_asmj.py (20201118)

--- python/test/auto_by_asmj.py
+++ python/test/auto_by_asmj.py
@@ -7,8 +7,6 @@ front = 270
 back  = 90
 left  = 0
 right = 180
-running = True
-
 print('conn lidar')
 lidar = MyLidar()
 lidar.connect()
@@ -18,25 +16,26 @@ micon = Micon()
 micon.connect(force=1)
 micon.start()
 
-while running == True:
-    go_front(30,500,front - 10)
-    running = False
-
 def go_front(target,velocity,sight) :
-    done = True
-    while done == False :
+    while 1 :
         if lidar.data[sight] * abs(math.cos(sight - 270)) >= target :
-            micon.write(F'[V,%d,%d]' % (velocity,velocity))
+            micon.write(F'[V,{velocity},{velocity}]')
         else:
-            done = True
+            break
         sleep(0.1)
     micon.write(F'[V,0,0]')
 
 def go_back(target,velocity,sight) :
-    done = True
-    while done == False :
+    while 1 :
         if lidar.data[sight] * abs(math.cos(sight - 270)) >= target :
-            micon.write(F'[V,%d,%d]' % ( -1*velocity, -1*velocity))
+            micon.write(F'[V,{-velocity},{-velocity}]')
         else:
-            done = True
+            break
     micon.write(F'[V,0,0]')
+
+if __name__ == "__main__":
+
+    running = True
+    while running == True:
+        go_front(30,500,front-10)
+        running = False
